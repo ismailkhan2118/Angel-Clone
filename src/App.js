23522099c48aa12b3a710abe3bb40526b641ca53
Mohammad Abrar <mjece39@gmail.com>
2022-05-27 01:45:49 +0530

    profile initial work

--- src/App.js
+++ src/App.js
@@ -1,27 +1,29 @@
-import "primereact/resources/themes/lara-light-indigo/theme.css";  
+import "primereact/resources/themes/lara-light-indigo/theme.css";
 import "primereact/resources/primereact.min.css";
-import "primeicons/primeicons.css";                                
+import "primeicons/primeicons.css";
 
-import { BrowserRouter, Link, Route, Routes } from 'react-router-dom';
-import './App.scss';
-import { Button } from 'primereact/button';
-
- 
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import "./App.scss";
+import { Button } from "primereact/button";
+import ProfilePage from "./pages/ProfilePage";
 
 function App() {
-	return (
-		<BrowserRouter>
-			<div className="App">
-				<button className='tw-bg-orange-600 tw-text-white tw-p-2 tw-mr-4'>Test Tailwind Button</button>
-				<Button label="Prime Button"  />
+  return (
+    <BrowserRouter>
+      <div className="App">
+        <button className="tw-bg-orange-600 tw-text-white tw-p-2 tw-mr-4">
+          Test Tailwind Button
+        </button>
+        <Button label="Prime Button" />
 
-				<Routes>
-					{/* <Route path='/123' element={null}></Route> */}
-				</Routes>
+        <Routes>
+          <Route path="/profile" element={<ProfilePage />} />
 
-			</div>
-		</BrowserRouter>
-	);
+          {/* <Route path='/123' element={null}></Route> */}
+        </Routes>
+      </div>
+    </BrowserRouter>
+  );
 }
 
 export default App;
