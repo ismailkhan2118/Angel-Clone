3ec72aa7537740ca0a66e6731259526e66f87ee0
munawar <syedmunawarrizwan17@gmail.com>
2022-06-29 20:32:11 +0530

    loginpage added

--- src/pages/SignUpPage.js
+++ src/pages/SignUpPage.js
@@ -1,5 +1,4 @@
 import React from 'react';
-import LoginPageForm from '../components/LoginPageForm';
 import LoginPageImg from '../components/LoginPageImg';
 import SignUpPageForm from '../components/SignUpPageForm';
 import "./SignUpPage.scss"
