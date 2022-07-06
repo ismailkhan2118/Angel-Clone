0aef8fb0831d9492ee4c199a8349275dd07726ce
Mohammad Abrar <mjece39@gmail.com>
2022-07-06 19:33:52 +0530

    added signup in  thunk

--- src/components/LoginPageForm.js
+++ src/components/LoginPageForm.js
@@ -1,54 +1,81 @@
-import React, { useEffect, useState } from 'react';
-import "./LoginPageForm.scss"
+import React, { useEffect, useState } from "react";
+import "./LoginPageForm.scss";
 import { NavLink, useNavigate } from "react-router-dom";
-import { loginAction } from '../store/auth-state';
-import { useDispatch, useSelector } from 'react-redux';
+import { loginAction } from "../store/auth-state";
+import { useDispatch, useSelector } from "react-redux";
 
 function LoginPageForm(props) {
-    const navigate = useNavigate()
-    const [email, setEmail] = useState("");
-    const [password, setPassword] = useState("");
-    const dispatch = useDispatch();
-    const userData = useSelector(state => state.auth);
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const dispatch = useDispatch();
+  const userData = useSelector((state) => state.auth);
 
-    useEffect(() => {
-        let auth_data = localStorage.getItem('user_data');
-        if (auth_data) {
-            auth_data = JSON.parse(auth_data);
-            if (auth_data.isAuthenticated) {
-                navigate('/home/profile');
-            }
-        } else if (!userData.pending && userData.isAuthenticated) {
-            localStorage.setItem('user_data', JSON.stringify(userData))
-            navigate('/home/profile');
-        }
-    }, [userData])
-
-    const loginHandler = () => {
-        dispatch(loginAction({
-            email: email,
-            password: password
-        }))
+  useEffect(() => {
+    let auth_data = localStorage.getItem("user_data");
+    if (auth_data) {
+      auth_data = JSON.parse(auth_data);
+      if (auth_data.isAuthenticated) {
+        navigate("/home/profile");
+      }
+    } else if (!userData.pending && userData.isAuthenticated) {
+      localStorage.setItem("user_data", JSON.stringify(userData));
+      navigate("/home/profile");
     }
+  }, [userData]);
 
-    return (
-        <div className='login-page-form-container'>
-            <div className='login-h1-div'>
-                <img src={"https://angel.co/assets/shared/angellist_logo_hand-242554a1b6fa9f574f4ee1488f18d011b3066c90989f8320c15961aedaada679.svg"} alt="anglelistlogo" />
-                <h1 className='login-h1'>Login</h1>
-                <p>Find the job made for you !</p>
-            </div>
-            <div className='login-input-div'>
-                <input className='login-input' type="email" placeholder='Email' value={email} onChange={(e) => { setEmail(e.target.value) }}></input>
-                <input className='login-input' type="password" placeholder='Password' value={password} onChange={(e) => { setPassword(e.target.value) }}></input>
-                <button className='login-btn' onClick={loginHandler}>Login</button>
-            </div>
-            <div className='login-signup-div'>
-                <p><b>Not Registerd ? </b>  </p>
-                <NavLink to='/signup'>Create an Account</NavLink>
-            </div>
-        </div>
+  const loginHandler = () => {
+    dispatch(
+      loginAction({
+        email: email,
+        password: password,
+      })
     );
+  };
+
+  return (
+    <div className="login-page-form-container">
+      <div className="login-h1-div">
+        <img
+          src={
+            "https://angel.co/assets/shared/angellist_logo_hand-242554a1b6fa9f574f4ee1488f18d011b3066c90989f8320c15961aedaada679.svg"
+          }
+          alt="anglelistlogo"
+        />
+        <h1 className="login-h1">Login</h1>
+        <p>Find the job made for you !</p>
+      </div>
+      <div className="login-input-div">
+        <input
+          className="login-input"
+          type="email"
+          placeholder="Email"
+          value={email}
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+        ></input>
+        <input
+          className="login-input"
+          type="password"
+          placeholder="Password"
+          value={password}
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+        ></input>
+        <button className="login-btn" onClick={loginHandler}>
+          Login
+        </button>
+      </div>
+      <div className="login-signup-div">
+        <p>
+          <b>Not Registerd ? </b>{" "}
+        </p>
+        <NavLink to="/signup">Create an Account</NavLink>
+      </div>
+    </div>
+  );
 }
 
 export default LoginPageForm;
--- src/components/SignUpPageForm.js
+++ src/components/SignUpPageForm.js
@@ -1,31 +1,97 @@
-import React from 'react';
-import "./SignUpForm.scss"
-import { NavLink, } from "react-router-dom"
+import React, { useEffect, useState } from "react";
+import "./SignUpForm.scss";
+import { NavLink, useNavigate } from "react-router-dom";
+import { signupAction } from "../store/auth-state";
+import { useDispatch, useSelector } from "react-redux";
 
 function SignUpPageForm(props) {
+  const navigate = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [fullName, setFullName] = useState("");
+  const dispatch = useDispatch();
+  const userData = useSelector((state) => state.auth);
 
-    return (
-        <div className='singupform-container'>
-            <div className='signup-h1-div'>
-                <img src={"https://angel.co/assets/shared/angellist_logo_hand-242554a1b6fa9f574f4ee1488f18d011b3066c90989f8320c15961aedaada679.svg"} alt="anglelistlogo" />
-                <h1 className='signup-h1'>Create Account</h1>
-                <p>Find your next opportunity  !</p>
-            </div>
-            <div className='signup-input-div'>
-                <label htmlFor='fullname'>Full Name</label>
-                <input id="fullname" className='signup-input' type="text" placeholder='   enter text'></input>
-                <label htmlFor='email'>Email</label>
-                <input id="email" className='signup-input' type="email" placeholder='   Email'></input>
-                <label htmlFor='password' >Password</label>
-                <input className='signup-input' type="password" placeholder='   Password'></input>
-                <button className='signup-btn'>SignUp</button>
-            </div>
-            <div className='login-signup-div'>
-                <p><b>Already have  an Account ? </b>  </p>
-                <NavLink to="/login">Log in </NavLink>
-            </div>
-        </div>
+  useEffect(() => {
+    let auth_data = localStorage.getItem("user_data");
+    if (auth_data) {
+      auth_data = JSON.parse(auth_data);
+      if (auth_data.isAuthenticated) {
+        navigate("/home/profile");
+      }
+    } else if (!userData.pending && userData.isAuthenticated) {
+      localStorage.setItem("user_data", JSON.stringify(userData));
+      navigate("/home/profile");
+    }
+  }, [userData]);
+
+  const signupHandler = () => {
+    dispatch(
+      signupAction({
+        fullName: fullName,
+        email: email,
+        password: password,
+      })
     );
+  };
+
+  return (
+    <div className="singupform-container">
+      <div className="signup-h1-div">
+        <img
+          src={
+            "https://angel.co/assets/shared/angellist_logo_hand-242554a1b6fa9f574f4ee1488f18d011b3066c90989f8320c15961aedaada679.svg"
+          }
+          alt="anglelistlogo"
+        />
+        <h1 className="signup-h1">Create Account</h1>
+        <p>Find your next opportunity !</p>
+      </div>
+      <div className="signup-input-div">
+        <label htmlFor="fullname">Full Name</label>
+        <input
+          id="fullname"
+          className="signup-input"
+          type="text"
+          placeholder=" enter text"
+          value={fullName}
+          onChange={(e) => {
+            setFullName(e.target.value);
+          }}
+        ></input>
+        <label htmlFor="email">Email</label>
+        <input
+          id="email"
+          className="signup-input"
+          type="email"
+          placeholder="   Email"
+          value={email}
+          onChange={(e) => {
+            setEmail(e.target.value);
+          }}
+        ></input>
+        <label htmlFor="password">Password</label>
+        <input
+          className="signup-input"
+          type="password"
+          placeholder="   Password"
+          value={password}
+          onChange={(e) => {
+            setPassword(e.target.value);
+          }}
+        ></input>
+        <button className="signup-btn" onClick={signupHandler}>
+          SignUp
+        </button>
+      </div>
+      <div className="login-signup-div">
+        <p>
+          <b>Already have an Account ? </b>{" "}
+        </p>
+        <NavLink to="/login">Log in </NavLink>
+      </div>
+    </div>
+  );
 }
 
 export default SignUpPageForm;
